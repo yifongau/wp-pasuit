eeb38a22bd20e1f99b5b422ed1d5398625bcbc8e
Yi Fong Au <yifongau@gmail.com>
2023-03-04 10:59:28 +0000

    Hardcoded navbar

--- themes/pasuit/header.php
+++ themes/pasuit/header.php
@@ -23,9 +23,11 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'pasuit' ); ?></a>
+  <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'pasuit' ); ?></a>
 
-	<header id="masthead" class="navigation">
+<!-- 
+  <header id="masthead" class="navigation">
+
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
@@ -35,7 +37,7 @@
 				<?php
 			else :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
 			endif;
 			$pasuit_description = get_bloginfo( 'description', 'display' );
@@ -43,10 +45,12 @@
 				?>
 				<p class="site-description"><?php echo $pasuit_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 			<?php endif; ?>
-		</div><!-- .site-branding -->
+		</div>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'pasuit' ); ?></button>
+    <nav id="site-navigation" class="navbar navbar-expand-lg navbar-light">
+
+
+			<button class="navbar-toggler border-0" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'pasuit' ); ?></button>
 			<?php
 			wp_nav_menu(
 				array(
@@ -55,5 +59,44 @@
 				)
 			);
 			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		</nav>
+  </header>
+-->
+
+<!-- HARDCODED NAVBAR -->
+<header class="navigation">
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="index.html"><img class="img-fluid" src="images/logo.png" alt="parsa"></a>
+    <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navogation"
+      aria-controls="navogation" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse text-center" id="navogation">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown">
+          <a class="nav-link text-uppercase text-dark dropdown-toggle" href="#" id="navbarDropdown"
+            role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Home
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="index.html">Homepage 1</a>
+            <a class="dropdown-item" href="homepage-2.html">Homepage 2</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-uppercase text-dark" href="about.html">Over pasuit</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-uppercase text-dark" href="contact.html">Contact</a>
+        </li>
+      </ul>
+      <form class="form-inline position-relative ml-lg-4">
+        <input class="form-control px-0 w-100" type="search" placeholder="Search">
+        <!-- <button class="search-icon" type="submit"><i class="ti-search text-dark"></i></button> -->
+        <a href="search.html" class="search-icon"><i class="ti-search text-dark"></i></a>
+      </form>
+    </div>
+  </nav>
+</header>
+
