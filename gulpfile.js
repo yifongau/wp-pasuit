085724e3cce1359998faa4608899a405d63ac598
Yi Fong Au <yifongau@gmail.com>
2023-05-10 09:49:44 +0000

    Cleaned gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,10 @@ const sass = require("gulp-sass")(require("sass"));
 const gulp = require("gulp");
 const gutil = require("gulp-util");
 const sourcemaps = require("gulp-sourcemaps");
-const fileinclude = require("gulp-file-include");
 const autoprefixer = require("gulp-autoprefixer");
-const bs = require("browser-sync").create();
 const rimraf = require("rimraf");
-const comments = require("gulp-header-comment");
 const jshint = require("gulp-jshint");
 const notify = require("gulp-notify");
-const plumber = require("gulp-plumber");
 
 var path = {
   src: {
@@ -20,36 +16,9 @@ var path = {
     scss: "scss/**/*.scss",
   },
   build: {
-    dirBuild: "theme/",
-    dirDev: "theme/",
+    dirBuild: "./",
   },
 };
-
-// HTML
-gulp.task("html:build", function () {
-  return gulp
-    .src(path.src.html)
-    .pipe(customPlumber("Error Running html-include"))
-    .pipe(
-      fileinclude({
-        basepath: path.src.incdir,
-      })
-    )
-    .pipe(
-      comments(`
-    WEBSITE: https://themefisher.com
-    TWITTER: https://twitter.com/themefisher
-    FACEBOOK: https://www.facebook.com/themefisher
-    GITHUB: https://github.com/themefisher/
-    `)
-    )
-    .pipe(gulp.dest(path.build.dirDev))
-    .pipe(
-      bs.reload({
-        stream: true,
-      })
-    );
-});
 
 // SCSS
 gulp.task("scss:build", function () {
@@ -63,20 +32,7 @@ gulp.task("scss:build", function () {
     )
     .pipe(autoprefixer())
     .pipe(sourcemaps.write("/"))
-    .pipe(
-      comments(`
-    WEBSITE: https://themefisher.com
-    TWITTER: https://twitter.com/themefisher
-    FACEBOOK: https://www.facebook.com/themefisher
-    GITHUB: https://github.com/themefisher/
-    `)
-    )
     .pipe(gulp.dest(path.build.dirDev + "css/"))
-    .pipe(
-      bs.reload({
-        stream: true,
-      })
-    );
 });
 
 // Javascript
@@ -95,27 +51,7 @@ gulp.task("js:build", function () {
     )
     .pipe(jshint.reporter("jshint-stylish"))
     .on("error", gutil.log)
-    .pipe(
-      comments(`
-  WEBSITE: https://themefisher.com
-  TWITTER: https://twitter.com/themefisher
-  FACEBOOK: https://www.facebook.com/themefisher
-  GITHUB: https://github.com/themefisher/
-  `)
-    )
     .pipe(gulp.dest(path.build.dirDev + "js/"))
-    .pipe(
-      bs.reload({
-        stream: true,
-      })
-    );
-});
-
-// Images
-gulp.task("images:build", function () {
-  return gulp
-    .src(path.src.images)
-    .pipe(gulp.dest(path.build.dirDev + "images/"))
     .pipe(
       bs.reload({
         stream: true,
@@ -147,11 +83,6 @@ gulp.task("plugins:build", function () {
     );
 });
 
-// Other files like favicon, php, sourcele-icon on root directory
-gulp.task("others:build", function () {
-  return gulp.src(path.src.others).pipe(gulp.dest(path.build.dirDev));
-});
-
 // Clean Build Folder
 gulp.task("clean", function (cb) {
   rimraf("./theme", cb);
@@ -179,28 +110,6 @@ gulp.task("watch:build", function () {
   gulp.watch(path.src.fonts, gulp.series("fonts:build"));
   gulp.watch(path.src.plugins, gulp.series("plugins:build"));
 });
-
-// Dev Task
-gulp.task(
-  "default",
-  gulp.series(
-    "clean",
-    "html:build",
-    "js:build",
-    "scss:build",
-    "images:build",
-    "fonts:build",
-    "plugins:build",
-    "others:build",
-    gulp.parallel("watch:build", function () {
-      bs.init({
-        server: {
-          baseDir: path.build.dirDev,
-        },
-      });
-    })
-  )
-);
 
 // Build Task
 gulp.task(
